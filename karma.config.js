e4542ede9666d98bddd656fb66d4131ce0a12127
Kevin King <kevinking@Kevins-MacBook-Pro.local>
2014-06-09 18:29:08 -0700

    switched to phantom js for test runner

--- karma.config.js
+++ karma.config.js
@@ -2,6 +2,6 @@ module.exports = function(config) {
     config.set({
         basePath: './'
         , frameworks: ['jasmine']
-        , browsers: ['Chrome']
+        , browsers: ['PhantomJS']
     })
 }
